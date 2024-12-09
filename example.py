6d1bf3ebb88efa8fed057aba87b7ee63e79c3bfb
Muruganandham R <muruganandham.r249@changepond.com>
2024-12-09 10:29:21 +0530

    build

---
+++ example.py
@@ -0,0 +1,6 @@
+from emails_automation.automation.web_extraction import web_scrapy
+
+#     # url = "https://tcpdf.org/examples/example_041/"
+url = "https://www.princexml.com/samples/"
+
+web_scrapy(url)
